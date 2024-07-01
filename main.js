17e6dcb587a683461db425b03d273c55b3ec0c56
채병선 <chaebyungsun@chaebyeongseon-ui-iMac.local>
2024-07-02 03:16:10 +0900

    7/1

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ let computerNum = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let restButton = document.getElementById("reset-button");
+let chanceArea = document.getElementById("chance-area");
+let chances = 3;
+let gameOver = false;
+
 // document.getElementById : id로 선택
 // document.getElementsByClassName: class로 선택, 같은class가 여러개 있을 경우엔 모두 다 선택이 되서 배열에 저장된다
 // document.querySelector : id, class둘다 선택이 가능하고 좀더 디테일한 선택이 가능하다 참고로 선택가능한 값이 여러개가 있을경우 그중에 첫번째 태그 하나만 반환한다
 // document.querySelectorAll: 위에 document.querySelector와 같다 하지만 All에서 알수있듯이 선택된값 모두를 NodeList에 담아 반환한다
 
 playButton.addEventListener("click", play);
+restButton.addEventListener("click", reset);
 
 function pickRandomNum() {
   computerNum = Math.floor(Math.random() * 100) + 1;
@@ -16,6 +22,10 @@ function pickRandomNum() {
 
 function play() {
   let userValue = userInput.value;
+
+  chances--;
+  console.log("chances", chances);
+
   if (userValue < computerNum) {
     resultArea.textContent = "up!";
   } else if (userValue > computerNum) {
@@ -23,6 +33,22 @@ function play() {
   } else {
     resultArea.textContent = "정답입니다";
   }
+
+  chanceArea.textContent = `남은기회:${chances}`;
+
+  if (chances == 1) {
+    gameOver = true;
+  }
+  if (gameOver == true) {
+    playButton.disabled = true;
+  }
 }
 
+function reset() {
+  // user input 창이 초기화되고
+  userInput.value = "";
+  // 새로운 번호가 생기고
+  pickRandomNum();
+  resultArea.textContent = "게임을 시작하지";
+}
 pickRandomNum();
